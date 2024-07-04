f7eb0677b6b0dbd4a2c26ac746dee3f8ee5f1857
Ant <antsplat@gmail.com>
2024-07-04 16:51:33 -0400

    add curl test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <algorithm>
 #include <cstdlib>
 #include <chrono>
-#include <exception>
 #include <iostream>
 #include <random>
 #include <string>
+#include <curl/curl.h>
 
 // Grailsort implementation provided by Morwenn.
 // Original code at https://github.com/HolyGrailSortProject/Rewritten-Grailsort
@@ -16,43 +16,67 @@ struct GameData {
     std::vector<std::string> platforms;
     float reviewScore = 0;
 
-    static inline bool compareTitles(GameData* const lhs, GameData* const rhs) {
+    static bool compareTitles(const GameData *const lhs, const GameData *const rhs) {
         return (lhs->title < rhs->title);
     }
 
-    static inline bool compareScores(GameData* const lhs, GameData* const rhs) {
+    static bool compareScores(const GameData *const lhs, const GameData *const rhs) {
         return (lhs->reviewScore < rhs->reviewScore);
     }
 };
 
-class apiException : public std::runtime_error {
+class apiException final : public std::runtime_error {
 public:
-    apiException() : std::runtime_error("Moby API key missing") {}
+    apiException() : std::runtime_error("Moby API key missing") {
+    }
 };
 
 int main() {
-    const char* env_MobyKey = std::getenv("MOBY_KEY");
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+        /* example.com is redirected, so we tell libcurl to follow redirection */
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        /* Perform the request, res gets the return code */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+
+
+    const char *env_MobyKey = std::getenv("MOBY_KEY");
     if (!env_MobyKey) {
         throw apiException();
     }
-    using namespace std::chrono;
+
+    using std::chrono::duration_cast, std::chrono::milliseconds;
+    typedef std::chrono::steady_clock clock;
 
     std::random_device rd;
     std::mt19937 generator(rd());
-    std::uniform_real_distribution<float> reviewDistrib(1, 5);
-    std::uniform_int_distribution<char> charDistrib('0', 'z');
-    std::uniform_int_distribution<int> titleLengthDistrib(5, 25);
+    std::uniform_real_distribution reviewDistrib(1.0f, 5.0f);
+    std::uniform_int_distribution charDistrib('0', 'z');
+    std::uniform_int_distribution titleLengthDistrib(5, 25);
     std::vector<std::string> platforms = {"Nintendo Switch", "Xbox One X", "PlayStation 5", "PC", "macOS", "Linux"};
-    std::uniform_int_distribution<int> platformLengthDistrib(1, 6);
-    std::uniform_int_distribution<int> platformDistrib(0, 1);
+    std::uniform_int_distribution platformLengthDistrib(1, 6);
+    std::uniform_int_distribution platformDistrib(0, 1);
 
-    std::vector<GameData*> data;
+    std::vector<GameData *> data;
     for (int n = 0; n != 100000; ++n) {
-        auto* game = new GameData();
+        auto *game = new GameData();
         game->reviewScore = reviewDistrib(generator);
         for (int i = 0; i < titleLengthDistrib(generator); i++) {
             char randomChar = charDistrib(generator);
-            while ( (randomChar >= ':' && randomChar <= '@') || (randomChar >= '[' && randomChar <= '`') ) {
+            while ((randomChar >= ':' && randomChar <= '@') || (randomChar >= '[' && randomChar <= '`')) {
                 randomChar = charDistrib(generator);
             }
             game->title += randomChar;
@@ -60,20 +84,20 @@ int main() {
         data.push_back(game);
     }
     puts("Sorting by review score, then by title: ");
-    auto start = high_resolution_clock::now();
+    auto start = clock::now();
     std::stable_sort(data.begin(), data.end(), GameData::compareTitles);
     std::stable_sort(data.begin(), data.end(), GameData::compareScores);
-    auto elapsedTime = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+    auto elapsedTime = duration_cast<milliseconds>(clock::now() - start);
     std::cout << "data: \n";
     std::cout << "This took " << elapsedTime.count() << " milliseconds.\n";
 
     std::shuffle(data.begin(), data.end(), generator);
 
     std::cout << "data: \n";
-    start = high_resolution_clock::now();
+    start = clock::now();
     grailsort(data.begin(), data.end(), GameData::compareTitles);
     grailsort(data.begin(), data.end(), GameData::compareScores);
-    elapsedTime = duration_cast<milliseconds>(high_resolution_clock::now() - start);
+    elapsedTime = duration_cast<milliseconds>(clock::now() - start);
     std::cout << "This took " << elapsedTime.count() << " milliseconds.\n";
 
     std::cout << "first element " << data[0]->title << '\n';
