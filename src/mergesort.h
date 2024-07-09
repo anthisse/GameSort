c3a695fb02f399c8e50f61fd9126b64b892619f1
lana-daraiseh <114152781+lana-daraiseh@users.noreply.github.com>
2024-07-09 18:55:17 +0300

    Added merge sort files

---
+++ src/mergesort.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifndef MERGESORT_H
+#define MERGESORT_H
+
+
+#include <vector>
+#include "Game.h"
+
+#include <iostream>
+
+using namespace std;
+
+
+namespace ms {
+
+	// this functions starts the recursive merge sort
+	void mergeSort(vector<Game*>& games, bool(*lessThanComparator)
+		(const Game* lhs, const Game* rhs));
+
+	// merges 2 halves of the vector sorted previously; uses comparator to 
+	//			decide order of merging.
+	void _merge(vector<Game*>& games, int left, int mid, int right, 
+		bool (*lessThanComparator)(const Game* lhs, const Game* rhs));
+
+	// recursively divides the vector into halves until each segment length = 1
+	void _recursiveMergeSort(vector<Game*>& games, int left, int right, 
+		bool(*lessThanComparator)(const Game* lhs, const Game* rhs));
+
+
+
+}
+
+class mergesort
+{
+
+
+};
+
